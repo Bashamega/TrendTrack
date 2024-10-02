9329f416f3722c2b4b06d74762d999b4923f4f0f
kvtemadden <74623315+kvtemadden@users.noreply.github.com>
2024-10-02 22:12:15 +0100

    save to local storage if clicked

--- src/components/Repo.tsx
+++ src/components/Repo.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+
 import { GithubData } from "./Repos";
 
 interface RepoProps {
@@ -7,18 +8,40 @@ interface RepoProps {
 }
 
 const Repo: React.FC<RepoProps> = ({ data, key }) => {
+  const [viewed, setViewed] = useState(() => {
+    const storedRepos = localStorage.getItem("ttViewedRepos");
+    return storedRepos ? JSON.parse(storedRepos).includes(data.id) : false;
+  });
+
+  const handleOnClick = () => {
+    const existingRepos = localStorage.getItem("ttViewedRepos");
+    const repos = existingRepos ? JSON.parse(existingRepos) : [];
+
+    if (!repos.includes(data.id)) {
+      repos.push(data.id);
+      localStorage.setItem("ttViewedRepos", JSON.stringify(repos));
+    }
+
+    window.open(`https://github.com${data.name}`);
+    setViewed(true);
+  };
+
   return (
-    <a
-      href={"https://github.com" + data.name}
-      target="_blank"
-      rel="noopener noreferrer"
+    <div
       className="bg-slate-700 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
       key={key}
+      onClick={handleOnClick}
     >
-      <p className="text-xl font-semibold text-gray-200">{data.name}</p>
+      <div className="flex items-center justify-between">
+        <p className="text-xl font-semibold text-gray-200">{data.name}</p>
+        {viewed ? (
+          <p className="text-gray-400 text-sm font-medium">Viewed</p>
+        ) : null}
+      </div>
+
       <p className="text-gray-400">Stars: {data.stars}</p>
       <p className="text-gray-400">Forks: {data.forks}</p>
-    </a>
+    </div>
   );
 };
 
