9367cb27f33beb86fb58287c86860687ee043583
kvtemadden <74623315+kvtemadden@users.noreply.github.com>
2024-10-03 16:35:23 +0100

    add filter

--- src/app/repos/viewed/page.tsx
+++ src/app/repos/viewed/page.tsx
@@ -4,10 +4,25 @@ import Loader from "@/components/Loader";
 import Navbar from "@/components/Nav";
 import Repo from "@/components/Repo";
 import type { GithubData } from "@/components/Repos";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { useEffect, useState } from "react";
 
 export default function Page() {
   const [data, setData] = useState<GithubData[]>([]);
+  const [filteredData, setFilteredData] = useState<GithubData[] | [] | null>(
+    null,
+  );
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [selectedOption, setSelectedOption] = useState<string>("daily");
+  const [date, setDate] = useState<string | undefined>(
+    new Date().toISOString().split("T")[0],
+  );
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -17,18 +32,62 @@ export default function Page() {
     }
   }, []);
 
+  useEffect(() => {
+    if (data) {
+      const filtered = data.filter((repo) =>
+        repo.name.toLowerCase().includes(searchQuery.toLowerCase()),
+      );
+      setFilteredData(filtered);
+    }
+  }, [searchQuery, data]);
+
   return (
     <main>
       <Navbar />
-      {data ? (
-        data.length > 0 ? (
+      <div className="flex flex-row items-center gap-2 py-5 px-5">
+        <input
+          type="date"
+          className="bg-gray-800 hover:bg-gray-900 text-gray-200 font-bold py-3 px-4 rounded"
+          onChange={(e) => {
+            setDate(e.target.value);
+          }}
+          value={date}
+        />
+        <input
+          type="text"
+          placeholder="Search Repositories"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="bg-gray-800 hover:bg-gray-900 text-gray-200 font-bold py-3 px-4 rounded"
+        />
+        <Select
+          value={selectedOption}
+          onValueChange={(value) => {
+            setSelectedOption(value);
+          }}
+        >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Daily" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="daily" defaultChecked={true}>
+              Daily
+            </SelectItem>
+            <SelectItem value="weekly">Weekly</SelectItem>
+            <SelectItem value="monthly">Monthly</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+
+      {filteredData ? (
+        filteredData.length > 0 ? (
           <div className="grid gap-5 px-5 pt-5">
-            {data.map((item: GithubData, index: number) => (
+            {filteredData.map((item, index) => (
               <Repo data={item} key={index} show={true} />
             ))}
           </div>
         ) : (
-          <div className="w-screen flex flex-col items-center justify-center pt-4">
+          <div className="w-screen flex flex-col items-center justify-center">
             <h1 className=" text-2xl">No Data!</h1>
           </div>
         )
